aed956c755da04b805789b152d68f8470595814e
Adam Wójs <adam@wojs.pl>
2017-06-27 14:45:50 +0200

    Activiti\Client\Model\{IdentityLinkList,VariableList} implementation

--- src/Client/Model/IdentityLinkList.php
+++ src/Client/Model/IdentityLinkList.php
@@ -2,7 +2,26 @@
 
 namespace Activiti\Client\Model;
 
-class IdentityLinkList
+class IdentityLinkList implements \IteratorAggregate
 {
+    /**
+     * @var array
+     */
+    private $items;
 
+    public function __construct(array $items = [])
+    {
+        $this->items = [];
+        foreach ($items as $item) {
+            $this->items[] = new IdentityLink($item);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->items);
+    }
 }
--- src/Client/Model/VariableList.php
+++ src/Client/Model/VariableList.php
@@ -2,7 +2,26 @@
 
 namespace Activiti\Client\Model;
 
-class VariableList extends ValueObject
+class VariableList implements \IteratorAggregate
 {
+    /**
+     * @var array
+     */
+    private $items;
 
+    public function __construct(array $items = [])
+    {
+        $this->items = [];
+        foreach ($items as $item) {
+            $this->items[] = new Variable($item);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->items);
+    }
 }
