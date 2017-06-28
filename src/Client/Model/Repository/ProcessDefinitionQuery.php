89af109bb91511bcce1a1b689e10698e36563000
Adam Wójs <adam@wojs.pl>
2017-06-28 12:34:29 +0200

    Added missing properties in ProcessDefinitionQuery

--- src/Client/Model/Repository/ProcessDefinitionQuery.php
+++ src/Client/Model/Repository/ProcessDefinitionQuery.php
@@ -6,5 +6,18 @@ use Activiti\Client\Model\AbstractQuery;
 
 class ProcessDefinitionQuery extends AbstractQuery
 {
-
+    public $version;
+    public $name;
+    public $nameLike;
+    public $key;
+    public $keyLike;
+    public $resourceName;
+    public $resourceNameLike;
+    public $category;
+    public $categoryLike;
+    public $categoryNotEquals;
+    public $deploymentId;
+    public $startableByUser;
+    public $latest;
+    public $suspended;
 }
