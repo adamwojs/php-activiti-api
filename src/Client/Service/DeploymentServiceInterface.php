797d5e6db8332bcb411f8620af5dcc2ed22c41ea
Adam Wójs <adam@wojs.pl>
2017-08-19 22:13:57 +0200

    DocBlock for DeploymentServiceInterface

--- src/Client/Service/DeploymentServiceInterface.php
+++ src/Client/Service/DeploymentServiceInterface.php
@@ -2,15 +2,47 @@
 
 namespace Activiti\Client\Service;
 
+use Activiti\Client\Model\Deployment\Deployment;
+use Activiti\Client\Model\Deployment\DeploymentList;
 use Activiti\Client\Model\Deployment\DeploymentQuery;
 
 interface DeploymentServiceInterface
 {
+    /**
+     * Get list of deployments.
+     *
+     * @see https://www.activiti.org/userguide/#_list_of_deployments
+     *
+     * @param DeploymentQuery|null $query
+     * @return DeploymentList
+     */
     public function getDeploymentList(DeploymentQuery $query = null);
 
+    /**
+     * Get a deployment.
+     *
+     * @see https://www.activiti.org/userguide/#_get_a_deployment
+     *
+     * @param string $deploymentId The id of the deployment to get.
+     * @return Deployment
+     */
     public function getDeployment($deploymentId);
 
+    /**
+     * Create a new deployment.
+     *
+     * @see https://www.activiti.org/userguide/#_create_a_new_deployment
+     *
+     * @param resource $deployment
+     * @return Deployment
+     */
     public function createDeployment($deployment);
 
+    /**
+     * Delete a deployment.
+     *
+     * @param string $deploymentId The id of the deployment to delete.
+     * @return void
+     */
     public function delete($deploymentId);
 }
